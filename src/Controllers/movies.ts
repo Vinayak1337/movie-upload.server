7c71c8c86b95b31dfbb4c954e18310775ee34a6c
Vinayak1337 <vinayak111kumar@gmail.com>
2021-09-01 00:06:03 +0530

    updating controller

---
+++ src/Controllers/movies.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express"
+import { moviesModel } from "../Models/moviesModel"
+
+export const getMovies =  async (req: Request, res: Response) => {
+    try {
+        const { limit } = req.body
+
+        if (!limit) {
+            res.status(400).json('Please provide movies limit.')
+            return
+        }
+    
+        const movies = await moviesModel.find({}, { limit })
+    
+        res.status(200).json(movies)
+    } catch (error) {
+        res.status(500).json(`Something went wrong. ${error.message}`)
+    }
+}
+
+export const postMovie = async (req: Request, res: Response) => {
+    try {
+        const { movieName, releasedOn, language, thumbnailUrl, videoUrl } = req.body
+
+        if (!(movieName || releasedOn || language | thumbnailUrl || videoUrl )) {
+            res.status(400).json('Incomplete body provided')
+            return
+        }
+    
+        const movie = await new moviesModel({ movieName, releasedOn, language, thumbnailUrl, videoUrl }).save()
+
+        res.status(200).json(movie)
+    } catch (error) {
+        res.status(500).json(`Something went wrong. ${error.message}`)
+    }
+}
--- src/Routes/movies.ts
+++ src/Routes/movies.ts
@@ -1,40 +1,10 @@
 import { Router } from "express"
-import { moviesModel } from "../Models/moviesModel";
+import { getMovies, postMovie } from "../Controllers/movies"
 
 const moviesRouter = Router();
 
-moviesRouter.get('/', async (req, res) =>  {
-    try {
-        const { limit } = req.body
+moviesRouter.get('/', getMovies)
 
-        if (!limit) {
-            res.status(400).json('Please provide movies limit.')
-            return
-        }
-    
-        const movies = await moviesModel.find({}, { limit })
-    
-        res.status(200).json(movies)
-    } catch (error) {
-        res.status(500).json(`Something went wrong. ${error.message}`)
-    }
-})
-
-moviesRouter.post('/', async (req, res) => {
-    try {
-        const { movieName, releasedOn, language, thumbnailUrl, videoUrl } = req.body
-
-        if (!(movieName || releasedOn || language | thumbnailUrl || videoUrl )) {
-            res.status(400).json('Incomplete body provided')
-            return
-        }
-    
-        const movie = await new moviesModel({ movieName, releasedOn, language, thumbnailUrl, videoUrl }).save()
-
-        res.status(200).json(movie)
-    } catch (error) {
-        res.status(500).json(`Something went wrong. ${error.message}`)
-    }
-})
+moviesRouter.post('/', postMovie)
 
 export default moviesRouter
